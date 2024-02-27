4e5a0716833746c4d243252dbe7d63dd5f4aad5a
Jawad Ali <jawad.ali@repairdesk.co>
2024-02-27 12:23:51 +0500

    deleted extras

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,14 @@
+
 <script setup lang="ts">
-const online = useOnline()
+
 </script>
 
 <template>
-  <div>
-    <Logos mb-6 />
-    <Suspense>
-      <ClientOnly>
-        <PageView v-if="online" />
-        <div v-else text-gray:80>
-          You're offline
-        </div>
-      </ClientOnly>
-      <template #fallback>
-        <div italic op50>
-          <span animate-pulse>Loading...</span>
-        </div>
-      </template>
-    </Suspense>
-    <InputEntry />
+  <div class="text-gray:80 ___">
+home ha
   </div>
 </template>
+
+<style scoped>
+
+</style>
